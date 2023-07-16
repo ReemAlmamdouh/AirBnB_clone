d7b0554937f8ff8be889885adf786b627a888066
ReemAlmamdouh <reem.almamdouh@gmail.com>
2023-07-16 04:56:21 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,16 @@ class BaseModel:
     
     def __init__(self, *args, **kwargs):
         """Initialization"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         
     def __str__(self):
         """Returns string representation of BaseModel"""
